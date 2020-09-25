e959cff178732713b9a843a165156817bb68d4ae
DarhangeR <civayan@gmail.com>
2020-09-25 12:25:12 +0300

    Small Edit

---
+++ Accountant_Classic/localization.ru.lua
@@ -0,0 +1,70 @@
+if (GetLocale() == "ruRU") then
+-- 
+-- Header
+ACCLOC_TITLE		=	"Accountant Classic 2.5.05";
+ACCLOC_TOT_IN		=	"Всего доходов";
+ACCLOC_TOT_OUT		=	"Всего расходов";
+ACCLOC_NET			=	"Чистая Прибыль / Убыток";
+ACCLOC_NETLOSS		=	"Чистый убыток";
+ACCLOC_NETPROF		=	"Чистый доход";
+ACCLOC_SOURCE		=	"Источник";
+ACCLOC_IN			=	"Доходы";
+ACCLOC_OUT			=	"Расходы";
+ACCLOC_WEEKSTART	=	"Начало недели";
+ACCLOC_SUM			=	"Итого";
+ACCLOC_CHAR			=	"Персонаж";
+ACCLOC_MONEY		=	"Золото";
+ACCLOC_UPDATED		=	"Обновлено";
+
+-- Section Labels
+ACCLOC_LOOT			=	"Добыча";
+ACCLOC_QUEST		=	"Награда за задание";
+ACCLOC_MERCH		=	"Торговцы";
+ACCLOC_TRADE		=	"Обмен";
+ACCLOC_MAIL			=	"Почта";
+ACCLOC_TRAIN		=	"Расходы на обучение";
+ACCLOC_TAXI			=	"Распорядитель полетов";
+ACCLOC_OTHER		=	"Неизвестно откуда";
+ACCLOC_REPAIR		=	"Затраты на ремонт";
+ACCLOC_AUC			=	"Аукционный дом";
+
+-- Buttons
+ACCLOC_RESET		=	"Сброс";
+ACCLOC_OPTBUT		=	"Параметры";
+ACCLOC_EXIT			=	"Выход";
+
+-- Tabs
+ACCLOC_SESS			=	"Сессия";
+ACCLOC_DAY			=	"Cегодня";
+ACCLOC_WEEK			=	"Неделя";
+ACCLOC_TOTAL		=	"Все";
+ACCLOC_CHARS		=	"Все персонажи";
+
+-- Options
+ACCLOC_OPTS			=	"Настройки Accountant";
+ACCLOC_MINIBUT		=	"Показать значек на мини-карте";
+ACCLOC_BUTPOS		=	"Позиция на мини-карте";
+ACCLOC_STARTWEEK	=	"Начало недели";
+ACCLOC_WD_MON		=	"Понедельник";
+ACCLOC_WD_TUE		=	"Вторник";
+ACCLOC_WD_WED		=	"Среда";
+ACCLOC_WD_THU		=	"Четверг";
+ACCLOC_WD_FRI		=	"Пятница";
+ACCLOC_WD_SAT		=	"Суббота";
+ACCLOC_WD_SUN		=	"Воскресенье";
+ACCLOC_DONE			=	"Готово";
+
+-- Misc
+ACCLOC_RESET_CONF	=	"Вы уверены, что хотите сбросить данные";
+ACCLOC_NEWPROFILE	=	"Новый профиль Accountant Classic создан для";
+ACCLOC_LOADPROFILE	=	"Загрузить профиль Accountant Classic для";
+ACCLOC_LOADED		=	"Загружено";
+ACCLOC_GOLD			=	"з ";
+ACCLOC_SILVER		=	"с ";
+ACCLOC_CENT			=	"м";
+
+-- Key Bindings headers
+BINDING_HEADER_ACCOUNTANT	=	"Accountant";
+BINDING_NAME_ACCOUNTANTTOG	=	"Toggle Accountant";
+
+end
